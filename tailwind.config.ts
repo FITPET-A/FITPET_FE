998d1eaffc821d750fe2696d0ac44342b19353d6
tacowasabii <godseoril812@gmail.com>
2024-08-29 00:31:00 +0900

    chore: font-weight

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,6 +69,9 @@ const config: Config = {
       borderWidth: {
         1: "1px",
       },
+      fontWeight: {
+        regular: "400",
+      },
     },
     screens: {
       desktop: "1080px",
