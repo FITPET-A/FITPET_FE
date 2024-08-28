16007b4af387b5a299053fe3b5a43e5975327979
tacowasabii <godseoril812@gmail.com>
2024-08-29 00:38:54 +0900

    refactor: font

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,7 +3,7 @@ import AnimalInfoFormBox from "./components/AnimalInfoFormBox";
 export default function Home() {
   return (
     <div className="mt-4 flex flex-col px-7">
-      <div className="font-paperlogy text-[40px] text-white">
+      <div className="font-paperlogy-heading text-[40px] text-white">
         3초만에 무료로 <br />
         간편 견적서를 신청하세요!
       </div>
