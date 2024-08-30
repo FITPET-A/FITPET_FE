b30a7e3eb0c0e59bfe1c8e356e775b311ca767c3
2unhi <ironhak1106@gmail.com>
2024-08-30 18:09:47 +0900

    style: footer tailwind css 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,17 +2,18 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="md bg-grayscale-100 py-10">
-      <div className="container mx-auto mb-4 px-4">
+    <footer className="mt-8 flex justify-center bg-grayscale-100 text-md">
+      <div className="flex w-[1080px] flex-col">
         {/* 이용약관 및 개인정보처리방침 */}
-        <div className="mb-6 flex flex-col justify-between">
-          <p className="text-grayscale-00">
-            이용약관&nbsp;&nbsp;&nbsp;개인정보처리방침
-          </p>
+        <div className="gab-6 mt-8 flex">
+          <div className="flex space-x-6 font-semibold text-grayscale-00">
+            <p>이용약관</p>
+            <p>개인정보처리방침</p>
+          </div>
         </div>
 
         {/* 회사 정보 */}
-        <div className="mb-6 flex flex-col justify-between text-grayscale-40">
+        <div className="mt-6 flex flex-col gap-1 text-grayscale-40">
           <p>
             스마트커버 인슈어런스(주) | 사업자 등록번호: 704-86-02268 |
             서울특별시 강남구 테헤란로 107길 6 B1
@@ -27,27 +28,27 @@ function Footer() {
         </div>
 
         {/* 연락처 */}
-        <div className="mb-6 text-grayscale-40">
-          <p>Contact us</p>
-          <p className="mt-2">
-            010-5748-1800 (월-금: 9am-6pm) | contact@smartcoverins.co.kr
-          </p>
+        <div className="mt-8 flex flex-col gap-2 text-grayscale-40">
+          <p className="font-bold">Contact us</p>
+          <p>010-5748-1800 (월-금: 9am-6pm) | contact@smartcoverins.co.kr</p>
         </div>
 
         {/* 필수 안내 사항 */}
-        <div className="mb-6 text-grayscale-60">
-          <p>필수 안내 사항</p>
-          <p className="mt-2">
-            보험대리점: 스마트커버 인슈어런스(주) 협회등록번호: 제203120029호 본
-            광고는 광고심의기준을 준수하였으며, 유효기간은 심의일로부터
-            1년입니다.
-          </p>
-          <p>
-            보험계약자가 기존 보험계약을 해지하고 새로운 보험 계약을 체결하는
-            과정에서 1) 질병이력, 연령증가 등으로 가입이 거절되거나 보험료가
-            인상될 수 있습니다. 2) 가입 상품에 따라 새로운 면책기간 적용 및 보장
-            제한 등 기타 불이익이 발생할 수 있습니다.
-          </p>
+        <div className="mb-16 mt-8 flex flex-col gap-2 text-grayscale-60">
+          <p className="font-semibold">필수 안내 사항</p>
+          <div>
+            <p>
+              보험대리점: 스마트커버 인슈어런스(주) 협회등록번호: 제203120029호
+              본 광고는 광고심의기준을 준수하였으며, 유효기간은 심의일로부터
+              1년입니다.
+            </p>
+            <p>
+              보험계약자가 기존 보험계약을 해지하고 새로운 보험 계약을 체결하는
+              과정에서 1) 질병이력, 연령증가 등으로 가입이 거절되거나 보험료가
+              인상될 수 있습니다. 2) 가입 상품에 따라 새로운 면책기간 적용 및
+              보장 제한 등 기타 불이익이 발생할 수 있습니다.
+            </p>
+          </div>
         </div>
       </div>
     </footer>
