e4ae2135f4068e19ace8d693c00729e32bc9927b
tacowasabii <godseoril812@gmail.com>
2024-08-31 13:04:43 +0900

    feat: 스크롤 시 스타일 변화 기능

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,56 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Nav from "./Nav";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="flex h-[84px] items-center justify-center bg-primary-50">
+    <header
+      className={`fixed left-0 right-0 top-0 z-50 flex h-[84px] w-full items-center justify-center overflow-x-auto transition-colors duration-500 ${
+        isScrolled ? "bg-white shadow-md" : "bg-primary-50"
+      }`}
+    >
       <div className="flex w-[1080px] justify-between">
         <Link href="/">
-          <Image
-            src="/assets/sc_logo_white.webp"
-            width={155}
-            height={40}
-            alt="sc_logo_white"
-            priority
-          />
+          {isScrolled ? (
+            <Image
+              src="/assets/sc_logo_color.webp"
+              width={155}
+              height={40}
+              alt="sc_logo_color"
+              priority
+            />
+          ) : (
+            <Image
+              src="/assets/sc_logo_white.webp"
+              width={155}
+              height={40}
+              alt="sc_logo_white"
+              priority
+            />
+          )}
         </Link>
-        <Nav />
+        <Nav isScrolled={isScrolled} />
       </div>
     </header>
   );
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,33 +5,45 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const menuItems = [
-  { href: ROUTES.HOME, label: "간편 견적 받기" },
+  { href: ROUTES.QUICK_QUOTE, label: "간편 견적 받기" },
   { href: ROUTES.LIFESTYLE, label: "반려생활" },
   { href: ROUTES.FAQ, label: "자주 묻는 질문" },
   { href: ROUTES.PARTNERSHIP, label: "제휴 제안" },
 ];
 
-function Nav() {
+function Nav({ isScrolled }: { isScrolled: boolean }) {
   const pathname = usePathname();
 
   return (
     <nav className="flex items-center" aria-label="네비게이션">
-      <ul className="flex space-x-8 text-lg text-primary-10">
-        {menuItems.map((item) => (
-          <li key={item.label}>
-            <Link
-              href={item.href}
-              aria-label={`${item.label} 페이지로 이동`}
-              className={`${
-                pathname === item.href
-                  ? "font-semibold text-grayscale-00"
-                  : "hover:font-semibold hover:text-grayscale-00"
-              } `}
-            >
-              {item.label}
-            </Link>
-          </li>
-        ))}
+      <ul
+        className={`flex space-x-8 text-lg ${isScrolled ? "text-grayscale-50" : "text-primary-10"}`}
+      >
+        {menuItems.map((item) => {
+          let linkClass = "";
+
+          if (pathname === item.href) {
+            linkClass = isScrolled
+              ? "font-semibold text-black"
+              : "font-semibold text-grayscale-00";
+          } else {
+            linkClass = isScrolled
+              ? "hover:font-semibold hover:text-black"
+              : "hover:font-semibold hover:text-grayscale-00";
+          }
+
+          return (
+            <li key={item.label}>
+              <Link
+                href={item.href}
+                aria-label={`${item.label} 페이지로 이동`}
+                className={linkClass}
+              >
+                {item.label}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
