907ee8d743c14dee06418300978000702cbf9587
tacowasabii <godseoril812@gmail.com>
2024-09-01 03:35:09 +0900

    style: 스타일 변경

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,7 +3,7 @@ import AnimalInfoFormBox from "./components/AnimalInfoFormBox";
 
 export default function Home() {
   return (
-    <div className="relative mt-[170px] flex flex-col px-7">
+    <div className="relative mt-[170px] flex flex-col px-[40px]">
       <div className="font-paperlogy-heading text-[40px] text-white">
         3초만에 무료로 <br />펫 보험 견적서를 신청하세요!
       </div>
