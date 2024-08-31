373eab38474adab5fb524d6d73cdce19e036956b
tacowasabii <godseoril812@gmail.com>
2024-08-31 13:05:42 +0900

    style: 문구 수정

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,10 +2,9 @@ import AnimalInfoFormBox from "./components/AnimalInfoFormBox";
 
 export default function Home() {
   return (
-    <div className="mt-4 flex flex-col px-7">
+    <div className="mt-[184px] flex flex-col px-7">
       <div className="font-paperlogy-heading text-[40px] text-white">
-        3초만에 무료로 <br />
-        간편 견적서를 신청하세요!
+        3초만에 무료로 <br />펫 보험 견적서를 신청하세요!
       </div>
       <AnimalInfoFormBox />
     </div>
