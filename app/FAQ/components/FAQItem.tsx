51535d6d3a767d344b5852eda643f5e855891c51
2unhi <ironhak1106@gmail.com>
2024-09-03 13:52:17 +0900

    refactor: 화살표 이미지 수정

--- app/FAQ/components/FAQItem.tsx
+++ app/FAQ/components/FAQItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Image from "next/image";
+import { RightArrowIcon, DownArrowIcon } from "@public/svg/";
 
 interface FAQItemProps {
   question: string;
@@ -23,14 +23,11 @@ function FAQItem({ question, answer }: FAQItemProps) {
     >
       <div className="flex items-center justify-between">
         <p className="font-semibold text-grayscale-70">{`Q. ${question}`}</p>
-        <Image
-          src={
-            isOpen ? "svg/faq_answerButton.svg" : "/svg/faq_questionButton.svg"
-          }
-          alt={isOpen ? "Answer icon" : "Question icon"}
-          width={isOpen ? 24 : 24}
-          height={isOpen ? 24 : 24}
-        />
+        {isOpen ? (
+          <DownArrowIcon width={24} height={24} />
+        ) : (
+          <RightArrowIcon width={24} height={24} />
+        )}
       </div>
       <div
         className={`overflow-hidden transition-all duration-300 ${
--- public/svg/index.ts
+++ public/svg/index.ts
@@ -1,5 +1,13 @@
 import HamburgerIcon from "./hamburger.svg";
-import RightArrowIcon from "./rightArrow.svg";
+import RightDirectionIcon from "./rightDirection.svg";
 import CloseIcon from "./close.svg";
+import RightArrowIcon from "./rightArrow.svg";
+import DownArrowIcon from "./downArrow.svg";
 
-export { HamburgerIcon, RightArrowIcon, CloseIcon };
+export {
+  HamburgerIcon,
+  RightDirectionIcon,
+  CloseIcon,
+  RightArrowIcon,
+  DownArrowIcon,
+};
