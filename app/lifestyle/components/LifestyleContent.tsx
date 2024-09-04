ed9ea294bb3eccb5b3854ba723bff25b29f2eac3
2unhi <ironhak1106@gmail.com>
2024-09-05 02:54:54 +0900

    style: 수정 사항 반영

--- app/lifestyle/components/LifestyleContent.tsx
+++ app/lifestyle/components/LifestyleContent.tsx
@@ -14,18 +14,17 @@ export default function LifestyleContent({
   onClick,
 }: LifestyleContentProps) {
   return (
-    <div
-      className="mt-8 flex h-[225px] w-full cursor-pointer"
+    <button
+      type="button"
+      className="mt-8 flex h-[225px] w-full cursor-pointer text-left"
       onClick={onClick}
       onKeyDown={(e) => {
         if (e.key === "Enter") {
           onClick();
         }
       }}
-      role="button"
-      tabIndex={0}
     >
-      <div className="mr-6 mt-2 w-[608px] flex-col justify-center whitespace-pre-line">
+      <div className="mr-6 mt-2 w-[608px] flex-col whitespace-pre-line">
         <h2 className="text-2xl font-semibold text-grayscale-100">{title}</h2>
         <p className="mt-4 text-lg text-grayscale-60">{content}</p>
         <div className="mt-4 flex space-x-2 font-medium text-primary-50">
@@ -46,6 +45,6 @@ export default function LifestyleContent({
         width={400}
         height={225}
       />
-    </div>
+    </button>
   );
 }
